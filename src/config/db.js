eb635b119dab53949ae5d519f0b06a72157ecbe9
MoNRaSpGit <ju4nrsuarez@gmail.com>
2025-09-20 19:34:10 -0300

    dfsd

--- src/config/db.js
+++ src/config/db.js
@@ -1,12 +1,15 @@
-import mysql from 'mysql2/promise'
-import dotenv from 'dotenv'
+import mysql from "mysql2/promise"
+import dotenv from "dotenv"
 
 dotenv.config()
 
-export const db = await mysql.createConnection({
+export const db = mysql.createPool({
   host: process.env.DB_HOST,
   user: process.env.DB_USER,
   password: process.env.DB_PASS,
   database: process.env.DB_NAME,
-  port: process.env.DB_PORT
+  port: process.env.DB_PORT,
+  waitForConnections: true,
+  connectionLimit: 10,
+  queueLimit: 0
 })
