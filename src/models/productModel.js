bcb1428058e09642cd31c5d99bcfe4b1d1214c1b
MoNRaSpGit <ju4nrsuarez@gmail.com>
2025-10-11 02:11:43 -0300

    dsfds

--- src/models/productModel.js
+++ src/models/productModel.js
@@ -1,11 +1,11 @@
 import { db } from '../config/db.js'
 
 export async function findByBarcode(barcode) {
-  const [rows] = await db.query("SELECT * FROM products WHERE barcode = ?", [barcode])
+  const [rows] = await db.query("SELECT * FROM productos_test WHERE barcode = ?", [barcode])
   return rows[0] || null
 }
 
 export async function getAllProducts() {
-  const [rows] = await db.query("SELECT * FROM products")
+  const [rows] = await db.query("SELECT * FROM productos_test")
   return rows
 }
